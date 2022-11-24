e243500e5a935bf6107eaa46923ce0aab9815066
Raygunito <112569656+Raygunito@users.noreply.github.com>
2022-11-24 23:21:34 +0100

    Préparation dossier et sous dossier

---
+++ src/test/TestExtract.java
@@ -0,0 +1,21 @@
+package test;
+import extraction.*;
+import java.io.*;
+import java.nio.file.Path;
+public class TestExtract {
+    public static void main(String[] args) {
+        try {
+            Path dossierTravail= Path.of("C:\\Users\\Laurent\\Desktop\\VS Code\\test");
+            ExtractMeta.deleteDirectory(new File(dossierTravail.toString()));
+            ExtractMeta.unzip(new FileInputStream(new File("sujet_projet_L2-I_POO-java_2022-2023_v1.1.odt")), dossierTravail);
+            ExtractMeta.showMeta(dossierTravail);
+        } catch (FileNotFoundException e) {
+            
+            e.printStackTrace();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       
+    }
+}
