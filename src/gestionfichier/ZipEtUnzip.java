036fe1cb6f8e1a7c1bc6009c240f552e5675b655
Raygunito <112569656+Raygunito@users.noreply.github.com>
2022-11-28 22:30:47 +0100

    Update +Cleared exceptions Added more information in exceptions Created zip() method

--- src/gestionfichier/ZipEtUnzip.java
+++ src/gestionfichier/ZipEtUnzip.java
@@ -1,19 +1,21 @@
 package gestionfichier;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 /**
  * Cette classe permet de manipuler les fichiers zip et plus généralement les fichiers dans le sens brut
  */
 public class ZipEtUnzip {
     /**
     * <p>Dézip un fichier et le stocke dans un dossier (crée le dossier s'il n'existe pas)</p>
-    * <p><i>Probkème de l'attaque Zip Slip résolu en comparant le chemin avant et après normalisation (retire les "./" et "../")</i></p>
+    * <p><i>Problème de l'attaque Zip Slip résolu en comparant le chemin avant et après normalisation (retire les "./" et "../")</i></p>
     * @param is InputStream of the file/directory
     * @param targetDir Save all the files in this directory
     * @throws IOException tried to open a non-existant file or directory
@@ -24,7 +26,7 @@ public class ZipEtUnzip {
             for (ZipEntry ze; (ze = zipIn.getNextEntry()) != null; ) {
                 Path resolvedPath = targetDir.resolve(ze.getName()).normalize();
                 if (!resolvedPath.startsWith(targetDir)) {
-                    throw new RuntimeException("Entry with an illegal path: "+ ze.getName());
+                    throw new IOException("Entry with an illegal path: "+ ze.getName());
                 }
                 if (ze.isDirectory()) {
                     Files.createDirectories(resolvedPath);
@@ -52,5 +54,64 @@ public class ZipEtUnzip {
             }
         }
         return dossierAsupprimer.delete();
+    }
+
+    public static void zip(File file, Path Dir,ZipOutputStream os) throws IOException{
+        if (file.isHidden()) {
+            return;
+        }
+        if (file.isDirectory()) {
+            if (Dir.toString().endsWith("/")) {
+                os.putNextEntry(new ZipEntry(Dir.toString()));
+                os.closeEntry();
+            } else {
+                os.putNextEntry(new ZipEntry(Dir.toString() + "/"));
+                os.closeEntry();
+            }
+            File[] children = file.listFiles();
+            for (File childFile : children) {
+                zip(childFile, Path.of( Dir.toString() + "/" + childFile.getName()), os);
+            }
+            return;
+        }
+        FileInputStream fis = new FileInputStream(file);
+        ZipEntry zipEntry = new ZipEntry(Dir.toString());
+        os.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            os.write(bytes, 0, length);
+        }
+        fis.close();
+        
+    }
+    //TODO JAVADOC afire
+    public static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+        if (fileToZip.isHidden()) {
+            return;
+        }
+        if (fileToZip.isDirectory()) {
+            if (fileName.endsWith("/")) {
+                zipOut.putNextEntry(new ZipEntry(fileName));
+                zipOut.closeEntry();
+            } else {
+                zipOut.putNextEntry(new ZipEntry(fileName + "/"));
+                zipOut.closeEntry();
+            }
+            File[] children = fileToZip.listFiles();
+            for (File childFile : children) {
+                zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
+            }
+            return;
+        }
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileName);
+        zipOut.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+        fis.close();
     }
 }
--- src/test/TestExtract.java
+++ src/test/TestExtract.java
@@ -4,6 +4,7 @@ import gestionfichier.ZipEtUnzip;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.zip.ZipOutputStream;
 public class TestExtract {
     public static void main(String[] args) {
         try {
@@ -14,6 +15,16 @@ public class TestExtract {
             ExtractContent.showLink(dossierTravail);
             ExtractMeta.setTitle(dossierTravail, "Je suis beau");
             ExtractMeta.setSubject(dossierTravail, "JE SAIS PAS SI CA MARCHEEE");
+            //TODO aucune idée de comment ca marcje
+            String sourceFile = "impossibleQueCeDossierSAppelleCommeCaVraimentSiJLeTrouveCestChaud//";
+            FileOutputStream fos = new FileOutputStream("dirCompressed.odt");
+            ZipOutputStream zipOut = new ZipOutputStream(fos);
+            
+            File fileToZip = new File(sourceFile);
+            ZipEtUnzip.zipFile(fileToZip,fileToZip.getName(), zipOut);
+            zipOut.close();
+            fos.close();
+            ZipEtUnzip.supprDossier(dossierTravail.toFile());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
