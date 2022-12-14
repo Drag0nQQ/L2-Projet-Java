ba30d331e08bdb1c9914e6513c035678351275a1
Raygunito <112569656+Raygunito@users.noreply.github.com>
2022-12-14 12:49:48 +0100

    Removed useless code and iteration

--- src/test/EncoreuneTree.java
+++ src/test/EncoreuneTree.java
@@ -21,7 +21,7 @@ public class EncoreuneTree extends JFrame{
         creatFeuille(noeud, new File("C:/Users/Laurent/Desktop"));
         jtree=new JTree(noeud);
         scrollpane=new JScrollPane(jtree);
-
+        
         Container main= super.getContentPane();
         main.add(scrollpane);
         main.setPreferredSize(new Dimension(480,240));
@@ -32,14 +32,14 @@ public class EncoreuneTree extends JFrame{
         this.setVisible(true);
     }
     /**
-     * <p>Ajoute à l'arborescence les fichiers en .odt ou dossiers contenant un .odt</p>
-     * <p>Fonctionnement :<ul><li>si File est un .odt on l'ajoute au noeud</li> 
-     *  <li>si File est un dossier alors on appelle à nouveau creatFeuille </li>
-     *  <li>si le DTM de File est une feuille alors on a forcément un .odt
-     *  sinon ca veut dire qui a un fils qui contient un .odt</li></ul></p>
-     * @param noeud DefaultMutableTreeNode
-     * @param f File
-     */
+    * <p>Ajoute à l'arborescence les fichiers en .odt ou dossiers contenant un .odt</p>
+    * <p>Fonctionnement :<ul><li>si File est un .odt on l'ajoute au noeud</li> 
+    *  <li>si File est un dossier alors on appelle à nouveau creatFeuille </li>
+    *  <li>si le DTM de File est une feuille alors on a forcément un .odt
+    *  sinon ca veut dire qui a un fils qui contient un .odt</li></ul></p>
+    * @param noeud DefaultMutableTreeNode
+    * @param f File
+    */
     public void creatFeuille(DefaultMutableTreeNode noeud, File f){
         DefaultMutableTreeNode fil=new DefaultMutableTreeNode(f.getName());
         if (f.isFile()&&f.getName().endsWith(".odt")){
@@ -49,25 +49,16 @@ public class EncoreuneTree extends JFrame{
         if (f.isDirectory()) {
             File[] liste=f.listFiles();
             DefaultMutableTreeNode node=new DefaultMutableTreeNode(f.getName());
-            for (File iterateurFile : liste) {
-                creatNoeud(fil, iterateurFile);
-                creatFeuille( node, iterateurFile);
+            if (liste.length>0){
+                for (File iterateurFile : liste) {
+                    creatFeuille( node, iterateurFile);
+                }
             }
             if (!node.isLeaf()) {
                 noeud.add(node);
             }
-           
         }
         
-    }
-    /**
-     * Permet d'ajouter au DTM racine, le DTM File.
-     * @param racine
-     * @param file
-     */
-    public void creatNoeud(DefaultMutableTreeNode racine,File file){
-        DefaultMutableTreeNode fils=new DefaultMutableTreeNode(file.getName());
-        racine.add(fils);
     }
     public static void main(String[] args) {
         new EncoreuneTree();
