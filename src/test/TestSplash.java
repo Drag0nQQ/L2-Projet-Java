3b382e8f415fb6d38a4522db6f64b4c0bc7f2590
Raygunito <112569656+Raygunito@users.noreply.github.com>
2022-12-22 15:28:02 +0100

    suppression

--- src/test/TestSplash.java
+++ src/test/TestSplash.java
@@ -1,12 +1,7 @@
 package test;
 
-import javax.imageio.ImageIO;
-
 import GUIMeta.GUIMeta;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.InputStream;
-import java.net.URL;
 import java.awt.*;
 
 public class TestSplash {
@@ -19,12 +14,9 @@ public class TestSplash {
         
         try {
             ImageIcon icon = new ImageIcon(Toolkit.getDefaultToolkit().createImage(this.getClass().getResource(GUIMeta.splashscreen)));
-            System.out.println(TestSplash.class.getResource("TestSplash.class"));
             JLabel splash = new JLabel("", icon, SwingConstants.CENTER);
-            System.out.println("splash chargé");
             try {
                 window.getContentPane().add(splash);
-                System.err.println("splash ajouté");
             } catch (IllegalArgumentException e) {
                 System.err.println("splash refusé");
                 e.printStackTrace();
@@ -39,18 +31,6 @@ public class TestSplash {
         new GUIMeta();
         window.setVisible(false);
         window.dispose();
-    }
-    private ImageIcon getImgFromResource(String path){
-        System.err.println(path);
-        URL odtLien = TestSplash.class.getResource(path);
-        try {
-            assert odtLien != null;
-            BufferedImage icon = ImageIO.read(odtLien);
-            return new ImageIcon(icon);
-        } catch (Exception e){
-            System.err.println("PUTAIN ON LA PAS TROUVE");
-        }
-        return null;
     }
     public static void main(String[] args) throws InterruptedException {
         new TestSplash();
